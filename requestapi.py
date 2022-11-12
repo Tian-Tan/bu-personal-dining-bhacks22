1f5db7756a6bcc02c23f9fab4d724914c30db290
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2022-11-12 17:32:00 -0500

    added api to fetch food info

--- requestapi.py
+++ requestapi.py
@@ -1,14 +1,20 @@
-import requests, datetime
+import requests
 
-# BU has three dining halls
-dining_halls=['marciano', 'warren', 'west']
+# format the data this way:
+# dining_halls=['marciano', 'warren', 'west']
+# date = "YYYY-MM-DD"
+
 # gets the current date
-date = str(datetime.datetime.now())[0: 10]
+#date = str(datetime.datetime.now())[0: 10]
 
-# fetches the menu for today for each of the dining halls and save them to the corresponding text files
-for hall in dining_halls:
+# fetches the menu for the day called for the dining hall called save them to the corresponding text files
+
+def get_data(hall, date):
+    ''' creates a json file that stores the menu for the dining hall on the date specified
+        dining_halls=['marciano', 'warren', 'west']
+        date = "YYYY-MM-DD"
+    '''
     data = requests.get(f'https://www.bu.edu/dining/api/locations/{hall}/menu/{date}/').text
-    print(f'https://www.bu.edu/dining/api/locations/{hall}/menu/{date}/')
-    f = open(f"{hall}-menu-on-{date}.txt", "w")
+    f = open(f"data/{hall}-menu-on-{date}.json", "w")
     f.write(data)
     f.close()
